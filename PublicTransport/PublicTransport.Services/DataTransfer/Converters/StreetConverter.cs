959e74dcdf8d4457f1190656be69185fdadff9ad
bdach <dach.bartlomiej@gmail.com>
2016-12-03 11:07:55 +0100

    Adding accidentally omitted StreetConverter

---
+++ PublicTransport/PublicTransport.Services/DataTransfer/Converters/StreetConverter.cs
@@ -0,0 +1,34 @@
+﻿using PublicTransport.Domain.Entities;
+
+namespace PublicTransport.Services.DataTransfer.Converters
+{
+    public class StreetConverter : IConverter<Street, StreetDto>
+    {
+        private readonly IConverter<City, CityDto> _cityConverter;
+
+        public StreetConverter()
+        {
+            _cityConverter = new CityConverter();
+        }
+
+        public StreetDto GetDto(Street entity)
+        {
+            return new StreetDto
+            {
+                Id = entity.Id,
+                Name = entity.Name,
+                City = _cityConverter.GetDto(entity.City)
+            };
+        }
+
+        public Street GetEntity(StreetDto dto)
+        {
+            return new Street
+            {
+                Id = dto.Id,
+                Name = dto.Name,
+                CityId = dto.City?.Id ?? 0
+            };
+        }
+    }
+}
