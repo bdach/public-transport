3e27f2bfe98c0de5eed08ccc6794db0e3c002526
Tymon Felski <tymon.felski@gmail.com>
2016-12-22 23:23:58 +0100

    User entity migration

--- PublicTransport/PublicTransport.Domain/Entities/User.cs
+++ PublicTransport/PublicTransport.Domain/Entities/User.cs
@@ -17,6 +17,11 @@ namespace PublicTransport.Domain.Entities
         }
 
         /// <summary>
+        ///     Contains the full name of the user.
+        /// </summary>
+        public string FullName { get; set; }
+
+        /// <summary>
         ///     Contains the username (login) of the user.
         /// </summary>
         [Required(ErrorMessage = "The username is required.")]
@@ -27,6 +32,11 @@ namespace PublicTransport.Domain.Entities
         /// </summary>
         [Required(ErrorMessage = "The password is required.")]
         public string Password { get; set; }
+
+        /// <summary>
+        ///     Contains the latest session token granted to the user by OAuth.
+        /// </summary>
+        public string LatestToken { get; set; }
 
         /// <summary>
         ///     Returns a list of <see cref="Role"/>s assigned to the user.
